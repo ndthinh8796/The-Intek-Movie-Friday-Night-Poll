c3402fe8eab82caf74a515067ac5dd1a10ca30db
Guest <you@example.com>
2019-01-16 21:37:52 +0700

    early state

---
+++ pythonweb/moviepoll/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Question, Choice
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    question_list = Question.objects.order_by('pub_date')
+    return render(request,
+                  'moviepoll/index.html',
+                  {'question_list': question_list},
+                  )
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request,
+                  'moviepoll/detail.html',
+                  {'question': question},
+                 )
+
+
+def result(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'moviepoll/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return render(request,
+                      'moviepoll/result.html',
+                      {'question': question}
+                     )
+def helloWorld(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return HttpResponse(request.POST)
